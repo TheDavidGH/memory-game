de8ac466cc606025ed555242bf277e8c76d0dd31
TheDavidGH <lilopoes2002@gmail.com>
2022-02-02 12:44:37 +0100

    No  se si se me sube el zip, pero lo importante es el EX3 de las vidas :/

--- app.js
+++ app.js
@@ -55,6 +55,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const grid = document.querySelector('.grid')
   const resultDisplay = document.querySelector('#result')
+  // si este contador llega a 0 mueres
+  let vidaQueSeResta = 4
   let cardsChosen = []
   let cardsChosenId = []
   let cardsWon = []
@@ -75,11 +77,16 @@ document.addEventListener('DOMContentLoaded', () => {
     const cards = document.querySelectorAll('img')
     const optionOneId = cardsChosenId[0]
     const optionTwoId = cardsChosenId[1]
-    
-    if(optionOneId == optionTwoId) {
+    //printar la vida
+    let live = document.getElementById("live")
+
+    if (optionOneId == optionTwoId) {
       cards[optionOneId].setAttribute('src', 'images/blank.png')
       cards[optionTwoId].setAttribute('src', 'images/blank.png')
       alert('You have clicked the same image!')
+      //restar 1 vida por cada carta duplicada
+      vidaQueSeResta--
+      live.innerHTML = vidaQueSeResta
     }
     else if (cardsChosen[0] === cardsChosen[1]) {
       alert('You found a match')
@@ -92,11 +99,16 @@ document.addEventListener('DOMContentLoaded', () => {
       cards[optionOneId].setAttribute('src', 'images/blank.png')
       cards[optionTwoId].setAttribute('src', 'images/blank.png')
       alert('Sorry, try again')
+      //restar 1 vida por cada carta que no es igual
+      vidaQueSeResta--
+      live.innerHTML = vidaQueSeResta
     }
+    if (vidaQueSeResta == 0)
+      alert("Fin del juego David Jorda perdiste 4 Vidas")
     cardsChosen = []
     cardsChosenId = []
     resultDisplay.textContent = cardsWon.length
-    if  (cardsWon.length === cardArray.length/2) {
+    if (cardsWon.length === cardArray.length / 2) {
       resultDisplay.textContent = 'Congratulations! You found them all!'
     }
   }
@@ -107,7 +119,7 @@ document.addEventListener('DOMContentLoaded', () => {
     cardsChosen.push(cardArray[cardId].name)
     cardsChosenId.push(cardId)
     this.setAttribute('src', cardArray[cardId].img)
-    if (cardsChosen.length ===2) {
+    if (cardsChosen.length === 2) {
       setTimeout(checkForMatch, 500)
     }
   }
